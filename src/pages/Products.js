b41d126255ea34c65f1d42edf3509d718c2bf24c
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-30 15:06:52 +0530

    Adding Dynamic path to Route

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 const Products = () => {
-  return <h1>The Product Page</h1>;
+  return (
+    <section>
+      <h1>The Product Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An online course</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
